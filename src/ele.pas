9012092507da2e2c77fefcd8d3e565fec7afb21f
fredvs <fiens@hotmail.com>
2024-11-12 21:27:19 +0100

    Some retouch in alarm and edit event forms.

--- src/ele.pas
+++ src/ele.pas
@@ -55,13 +55,33 @@ procedure telefo.onclose(const sender: TObject);
 begin
 efelefo := false;
 end;
+
 procedure telefo.Display;
-var f : int64;
+var
+ f : int64;
+ i, i2 : integer;
+ str: msestring;
 begin
 tstringgrid1.rowcount := Length(org.w);
 for f := 0 to High(org.w) do begin
 //tstringgrid1[0].items[f] := IntToStr(f + 1) + '. ' +   org.w[f].DisplayString;
-tstringgrid1[0].items[f] := IntToStr(f + 1) + '. ' +   org.w[f].DisplayString;
+
+str := org.w[f].DisplayString;
+tstringgrid1[0].items[f] := IntToStr(f + 1);
+ //tstringgrid1[0].items[f] := IntToStr(f + 1) + '. ' +   org.w[f].DisplayString;
+
+i2 := 1;
+while (system.pos(',',str) > 0) and (i2 <11) do
+begin
+i := system.pos(',',str);
+tstringgrid1[i2].items[f] := system.copy(str, 1, i -1);
+//writeln(tstringgrid1[0].items[f]);
+str := system.copy(str, i+1,length(str));
+inc(i2);
+end;
+
+tstringgrid1[11].items[f] := system.copy(str, 1, i -1);
+// tstringgrid1[0].items[f] := IntToStr(f + 1) + '. ' +   org.w[f].DisplayString;
 ////tstringgrid1[0].items[f] := org.w[f].ExportString;
 end;
 end;
