f044aad5980e26d7cc562b202470063ca7435a14
fredvs <fiens@hotmail.com>
2024-11-18 00:33:58 +0100

    Yearlist form, the row of first day of month is light red.(3)

--- src/yearlist.pas
+++ src/yearlist.pas
@@ -77,11 +77,11 @@ c := 0;
   for f := 1 to 12 do
      for ff := 1 to md[f] do 
      begin
-        if ff = 1 then tstringgrid1.rowcolorstate[c]:= 1;
         adate :=  StrToDate(inttostr(ff) + '/'+  IntToStr(f) + '/'+ inttostr(aYear)); 
         day := ShortDayNames[DayOfWeek(aDate)];
         if DayOfWeek(aDate) = 1 then 
         tstringgrid1.rowcolorstate[c]:= 0;
+        if ff = 1 then tstringgrid1.rowcolorstate[c]:= 1;
         tstringgrid1.fixcols[-1].captions[c] := day + ' ' +
         {IntToStr(c+1) + '  ' +} IntToStr(ff) + ' ' + DefaultFormatSettings.ShortMonthNames[f] + '(' + IntToStr(f) + ')';
         Inc(c);
